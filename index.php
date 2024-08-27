1c7a00d871b1ef0bc8d403882d582039a8fb62cd
Your Name <adjienurlitiumquantum@gmail.com>
2024-08-27 11:36:55 +0700

    tugas11

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <form action="penjumlahan.php" method="get">
+        <label for="">Angka 1</label>
+        <input type="number" placeholder="angka 1" name="num1">
+        <br>
+        <label for="">Angka 2</label>
+        <input type="number" placeholder="angka 2" name="num2">
+        <button type="submit">Jumlahkan</button>
+    </form>
+</html>
